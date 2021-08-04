8a725325012608b77430be1932c0825f53049dea
irostub <irostub@gmail.com>
2021-08-05 08:22:17 +0900

    Fix main class name

--- src/main/java/com/irostub/studya/App.java
+++ src/main/java/com/irostub/studya/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyaApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyaApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
