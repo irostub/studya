d3158221b77ba48f8c0af41df72271fbafb367e0
irostub <irostub@gmail.com>
2021-08-05 07:02:17 +0900

    Init studya project

---
+++ src/main/java/com/irostub/studya/StudyaApplication.java
@@ -0,0 +1,13 @@
+package com.irostub.studya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irostub/studya/StudyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irostub.studya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
