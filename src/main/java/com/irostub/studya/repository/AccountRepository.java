7219adf3128edf5c628f3e1b4f958ff1e2cd3d34
irostub <irostub@gmail.com>
2021-08-07 21:20:40 +0900

    Fix return object Account to Optional<Account>
    in findByEmail()

--- src/main/java/com/irostub/studya/repository/AccountRepository.java
+++ src/main/java/com/irostub/studya/repository/AccountRepository.java
@@ -4,11 +4,13 @@ import com.irostub.studya.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
 
-    Account findByEmail(String email);
+    Optional<Account> findByEmail(String email);
 }
