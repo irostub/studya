0214e0843aa33c7a2e5c2f442cdcf8cd0657ea82
irostub <irostub@gmail.com>
2021-08-10 01:52:57 +0900

    Add log interceptor

---
+++ src/main/java/com/irostub/studya/interceptor/LogInterceptor.java
@@ -0,0 +1,30 @@
+package com.irostub.studya.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uuid = UUID.randomUUID().toString();
+        log.info("[preHandle] [{}] [{}] [{}]", uuid, request.getRequestURI(), handler);
+        request.setAttribute("logUuid", uuid);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("[postHandle] [{}] [{}] [{}]",request.getAttribute("logUuid"), request.getRequestURI(), modelAndView == null ? "null" : modelAndView.getViewName());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("[afterCompletion] [{}] [{}] [{}] [{}]", request.getAttribute("logUuid"), request.getRequestURI(), handler, ex);
+    }
+}
