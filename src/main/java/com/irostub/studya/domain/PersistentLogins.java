8a68b36407f3134c619f7f9ec3da7f47afae8ccf
irostub <irostub@gmail.com>
2021-08-10 02:19:45 +0900

    Implement remember me login cookie

---
+++ src/main/java/com/irostub/studya/domain/PersistentLogins.java
@@ -0,0 +1,30 @@
+package com.irostub.studya.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class PersistentLogins {
+    @NotNull
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @NotNull
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime lastUsed;
+}
