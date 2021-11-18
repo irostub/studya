ee886bef132c6ebb0f7e11d2736cc7da2e5168e9
irostub <irostub@gmail.com>
2021-11-18 11:58:37 +0900

    Implement notification serving logic

--- src/main/java/com/irostub/studya/modules/notification/NotificationRepository.java
+++ src/main/java/com/irostub/studya/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.irostub.studya.modules.notification;
 
+import com.irostub.studya.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
