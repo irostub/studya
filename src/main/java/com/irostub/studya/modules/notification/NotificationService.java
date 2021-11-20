ed770c2fb109cec3db99b0d8c15fb1747bdd10f1
irostub <irostub@gmail.com>
2021-11-20 21:52:02 +0900

    Update notification entity

---
+++ src/main/java/com/irostub/studya/modules/notification/NotificationService.java
@@ -0,0 +1,20 @@
+package com.irostub.studya.modules.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    public void markAsRead(List<Notification> notifications) {
+        notifications.forEach(notification -> notification.changeCheckedFlag(true));
+        notificationRepository.saveAll(notifications);
+    }
+}
