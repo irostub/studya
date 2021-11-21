eab6f6ab28e880f482d62d6feabf5b279fbb44de
irostub <irostub@gmail.com>
2021-11-22 08:31:19 +0900

    Implement enrollment event listener

---
+++ src/main/java/com/irostub/studya/modules/event/event/EnrollmentEvent.java
@@ -0,0 +1,15 @@
+package com.irostub.studya.modules.event.event;
+
+import com.irostub.studya.modules.event.Enrollment;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public abstract class EnrollmentEvent {
+
+    protected final Enrollment enrollment;
+
+    protected final String message;
+
+}
---
+++ src/main/java/com/irostub/studya/modules/event/event/EnrollmentRejectedEvent.java
@@ -0,0 +1,10 @@
+package com.irostub.studya.modules.event.event;
+
+import com.irostub.studya.modules.event.Enrollment;
+
+public class EnrollmentRejectedEvent extends EnrollmentEvent {
+
+    public EnrollmentRejectedEvent(Enrollment enrollment) {
+        super(enrollment, "모임 참가 신청을 거절했습니다.");
+    }
+}
