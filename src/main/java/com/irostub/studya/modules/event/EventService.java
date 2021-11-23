877128fcf7bbe8a59658fed7fc628996953f0c55
irostub <irostub@gmail.com>
2021-11-23 12:26:39 +0900

    Update event service,
    use event publisher
    study update event and enrollment event

--- src/main/java/com/irostub/studya/modules/event/EventService.java
+++ src/main/java/com/irostub/studya/modules/event/EventService.java
@@ -1,10 +1,14 @@
 package com.irostub.studya.modules.event;
 
+import com.irostub.studya.modules.event.event.EnrollmentAcceptedEvent;
+import com.irostub.studya.modules.event.event.EnrollmentRejectedEvent;
 import com.irostub.studya.modules.event.form.EventForm;
 import com.irostub.studya.modules.account.Account;
 import com.irostub.studya.modules.study.Study;
 import com.irostub.studya.modules.study.EnrollmentRepository;
+import com.irostub.studya.modules.study.event.StudyUpdateEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,23 +20,29 @@ public class EventService {
     private final EventRepository eventRepository;
     private final EnrollmentRepository enrollmentRepository;
     private final EventMapper eventMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public Event createEvent(Study study, Event event, Account account) {
         event.setStudy(study);
         event.setCreatedBy(account);
         event.setCreatedDateTime(LocalDateTime.now());
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                event.getTitle()+" 모임이 만들어졌습니다."));
         return eventRepository.save(event);
     }
 
     @Transactional
     public void updateEvent(Event event, EventForm eventForm) {
         eventMapper.updateFormToEntity(eventForm, event);
-        //이벤트 수정 시 늘어난 인원 만큼 선착순 인원을 확정상태로 변경
         event.acceptWaitingList();
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                event.getTitle() + "의 모임 정보가 수정되었습니다."));
     }
 
     public void deleteEvent(Event event) {
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(),
+                event.getTitle() + "의 모임이 삭제되었습니다."));
         eventRepository.delete(event);
     }
 
@@ -61,12 +71,14 @@ public class EventService {
     @Transactional
     public void acceptEnrollment(Event event, Long id) {
         Enrollment enrollment = enrollmentRepository.findById(id).orElseThrow();
+        eventPublisher.publishEvent(new EnrollmentAcceptedEvent(enrollment));
         event.accept(enrollment);
     }
 
     @Transactional
     public void rejectEnrollment(Event event, Long id) {
         Enrollment enrollment = enrollmentRepository.findById(id).orElseThrow();
+        eventPublisher.publishEvent(new EnrollmentRejectedEvent(enrollment));
         event.reject(enrollment);
     }
 }
