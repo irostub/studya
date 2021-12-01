534eb0a4883e1d3c50fbbc6cc33e9d25126ff6a7
irostub <irostub@gmail.com>
2021-12-01 19:48:59 +0900

    Add banner resource

--- src/main/java/com/irostub/studya/modules/main/HomeController.java
+++ src/main/java/com/irostub/studya/modules/main/HomeController.java
@@ -2,8 +2,11 @@ package com.irostub.studya.modules.main;
 
 import com.irostub.studya.modules.account.CurrentAccount;
 import com.irostub.studya.modules.account.Account;
+import com.irostub.studya.modules.study.Study;
 import com.irostub.studya.modules.study.StudyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +25,11 @@ public class HomeController {
     }
 
     @GetMapping("/search/study")
-    public String searchStudy(String keyword, Model model) {
-        model.addAttribute("studyList", studyRepository.findByKeyword(keyword));
+    public String searchStudy(String keyword, Pageable pageable, Model model) {
+        Page<Study> studyPage = studyRepository.findByKeyword(keyword, pageable);
         model.addAttribute("keyword", keyword);
-        return "/content/search/search";
+        model.addAttribute("studyPage",studyPage);
+        return "content/search/search";
     }
 
     @GetMapping("/login")
